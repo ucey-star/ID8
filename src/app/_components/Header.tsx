7d7c54999f44b22425e027a495e4a64b2e7bc483
matviiko <matvii@uni.minerva.edu>
2024-10-17 12:38:55 -0700

    react app setup

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="fixed left-0 right-0 top-0 z-50 border-b-2 border-gray-200 bg-white">
+      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+        <div className="text-xl font-bold text-gray-600">ID8</div>
+        <nav>
+          <ul className="flex flex-row gap-6">
+            <li className="text-gray-600">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="text-gray-600">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../_components/Header";
+
+export default function About() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <div className="flex flex-col gap-8 px-32 py-16">
+        <h1 className="text-4xl font-bold text-gray-600">About</h1>
+        <p className="text-xl font-medium text-gray-600">
+          This is the about page
+        </p>
+      </div>
+    </div>
+  );
+}
