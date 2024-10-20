59b11c29373076abcd66a0e818c2cea3ef0d182f
Flavia Iespa <flavia.iespa@uni.minerva.edu>
2024-10-20 13:35:49 -0700

    adds the guidelines and personal details page, button design, and contributions file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Header from "./_components/Header";
+import Link from 'next/link'; 
 
 export default function HomePage() {
 	const [counter, setCounter] = useState(0);
@@ -22,6 +23,21 @@ export default function HomePage() {
 			<div className="flex flex-col items-center justify-center rounded-2xl bg-slate-500 px-16 py-8">
 				{counter}
 			</div>
+      {/* New Section for Routing. Only added this to facilitate development while we don't have a main page and we want to see the other pages in a easy way. We will remove it later*/}
+      <div className="flex flex-col items-center justify-center gap-4 mt-4">
+        {/* Link to Guidelines Page */}
+        <Link href="/guidelines" passHref>
+          <Button variant="contained" color="primary">
+            Go to Guidelines
+          </Button>
+        </Link>
+        {/* Link to Personal Details Page */}
+        <Link href="/personal-details" passHref>
+          <Button variant="contained" color="primary">
+            Go to Personal Details
+          </Button>
+        </Link>
+      </div>
 		</main>
 	);
 }
