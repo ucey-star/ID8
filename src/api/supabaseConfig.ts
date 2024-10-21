e3c6972bde2564a4f4fb477fc9ac505e9a257b33
Lewis Kyron <kyronnyoro@uni.minerva.edu>
2024-10-20 17:13:15 -0700

    resolving merge_conflicts

--- src/api/supabaseConfig.ts
+++ src/api/supabaseConfig.ts
@@ -1,15 +1,18 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 import type { SupabaseClient } from "@supabase/supabase-js";
-import type {Database} from "../types/database.types"
+import type { Database } from "../types/database.types";
 
-
-const supabaseURL:string| undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey:string|undefined = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseURL: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: string | undefined =
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseURL || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables");
+	throw new Error("Missing Supabase environment variables");
 }
 
-const supabaseClient: SupabaseClient<Database> = createClient(supabaseURL, supabaseAnonKey);
+const supabaseClient: SupabaseClient<Database> = createClient(
+	supabaseURL,
+	supabaseAnonKey,
+);
 
 export default supabaseClient;
