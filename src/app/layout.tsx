0e63b67818e29c59a74a58c4e834c870f25e8772
matviiko <matvii@uni.minerva.edu>
2024-10-23 10:28:31 -0700

    prettier fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,24 @@ import { Outfit } from "next/font/google";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ID8",
-  description: "By Founders for Founder",
-  icons: [{ rel: "icon", url: "/id8.ico" }],
+	title: "ID8",
+	description: "By Founders for Founder",
+	icons: [{ rel: "icon", url: "/id8.ico" }],
 };
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  display: "swap",
-  variable: "--font-outfit",
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700", "800", "900"],
+	display: "swap",
+	variable: "--font-outfit",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${outfit.className}`}>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${outfit.className}`}>
+			<body>{children}</body>
+		</html>
+	);
 }
