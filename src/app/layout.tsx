6a5b9ae8f838e5b5d9706a05273993e85008800f
avalucianelson <ava@uni.minerva.edu>
2024-12-03 00:52:05 +0100

    Set up Google Analytics tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "~/styles/globals.css";
 import { Outfit } from "next/font/google";
-
 import { type Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "ID8",
-	description: "By Founders for Founder",
+	description: "By Founders for Founders",
 	icons: [{ rel: "icon", url: "/id8.ico" }],
 };
 
@@ -21,6 +21,22 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en" className={`${outfit.className}`}>
+			<head>
+				{/* Google tag (gtag.js) */}
+				<Script
+					id="google-analytics"
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-YCR5CFZNMZ"
+				/>
+				<Script id="google-analytics-init">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'G-YCR5CFZNMZ');
+					`}
+				</Script>
+			</head>
 			<body>{children}</body>
 		</html>
 	);
