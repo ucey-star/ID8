b128468c1c701c21986a6c866ee39b1fe41ae02f
avalucianelson <ava@uni.minerva.edu>
2024-12-03 01:01:44 +0100

    Add Hotjar tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,20 @@ export default function RootLayout({
 							gtag('config', 'G-YCR5CFZNMZ');
 					`}
 				</Script>
+
+				{/* Hotjar Tracking Code */}
+				<Script id="hotjar-tracking" strategy="lazyOnload">
+					{`
+						(function(h,o,t,j,a,r){
+							h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+							h._hjSettings={hjid:5228354,hjsv:6};
+							a=o.getElementsByTagName('head')[0];
+							r=o.createElement('script');r.async=1;
+							r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+							a.appendChild(r);
+						})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+					`}
+				</Script>
 			</head>
 			<body>{children}</body>
 		</html>
